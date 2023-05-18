0ff497c748a0c7a54bcaa6af153d2f69869f6103
mikerspencer <mikerspencer@pm.me>
2023-05-18 14:58:20 +0100

    commented out idgroup as needs massive ram

--- r/partition_data.R
+++ r/partition_data.R
@@ -32,12 +32,12 @@ ds %>%
 # ---------------------------------
 # ID group partition
 
-dir_out = "data_part_id"
-
-if(!dir.exists(dir_out)){
-  dir.create(dir_out)
-}
-
-ds %>%
-  group_by(str_sub(cid, 1, 1)) %>%
-  write_dataset(dir_out)
+# dir_out = "data_part_id"
+#
+# if(!dir.exists(dir_out)){
+#   dir.create(dir_out)
+# }
+#
+# ds %>%
+#   group_by(str_sub(cid, 1, 1)) %>%
+#   write_dataset(dir_out)
